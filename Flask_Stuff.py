54fdc7bb35233c6a4735a9ff350d29c146f6bce6
Lecherouscthulhu <42318215+Lecherouscthulhu@users.noreply.github.com>
2020-01-25 06:32:32 -0500

    First Commit
    This is my first commit of python code for this project

---
+++ Flask_Stuff.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import nltk
+import matplotlib
+
+# add functions here for testing
+def integer_thingy():
+    for i in range(5):
+        return i
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World</h1>"
+
+@app.route("/functions")
+def functions():
+    return '<h1>Your Integers are {}</h1>'.format(integer_thingy())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
