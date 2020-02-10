1c8bc15bedbbe357fd951a37147f768805b4b910
Lecherouscthulhu <42318215+Lecherouscthulhu@users.noreply.github.com>
2020-02-09 19:41:03 -0500

    Playing around
    Just messing around with moving data from the front end to the back end

--- Flask_Stuff.py
+++ Flask_Stuff.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+    return render_template('Bootstrappin.html')
+
+url = 'Bootstrappin.html'
+User_data = {'somekey':'somedata'}
 
 if __name__ == '__main__':
     app.run(debug=True)
