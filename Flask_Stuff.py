1db5ab57525eb45a0635646723343e10bd0e8d6b
Lecherouscthulhu <42318215+Lecherouscthulhu@users.noreply.github.com>
2020-02-14 20:01:30 -0500

    Added working forms
    With some help at the meetup i was able to get the forms to work

--- Flask_Stuff.py
+++ Flask_Stuff.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
-import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask_wtf.csrf import CSRFProtect
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
 
 app = Flask(__name__)
+app.config.update(
+        TESTING=True,
+        SECRET_KEY='onetwo34fif1'
+    )
+csrf = CSRFProtect()
 
-@app.route("/")
-def template_test():
-    return render_template('Bootstrappin.html')
-
-url = 'Bootstrappin.html'
-User_data = {'somekey':'somedata'}
+@app.route('/', methods=('GET', 'POST'))
+def contact():
+    csrf.init_app(app)
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('Bootstrappin.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
